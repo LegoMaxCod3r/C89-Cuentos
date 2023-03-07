a54383228cc2d56e8519e25b4d4ac4b127bd9be3
LegoMaxCod3r <93746195+LegoMaxCod3r@users.noreply.github.com>
2023-03-06 20:42:31 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB4-fkE8g6m-ykE4zt-5zfBskdM2C6NiWE",
+  authDomain: "dynamiclinkers-6d965.firebaseapp.com",
+  projectId: "dynamiclinkers-6d965",
+  storageBucket: "dynamiclinkers-6d965.appspot.com",
+  messagingSenderId: "95959171039",
+  appId: "1:95959171039:web:839596d1ef9050cdfa0e6a",
+  measurementId: "G-5PEV373MPV"
+};
+
